d23e3f0be1c123e47d4d73912d5685f0e74fd4ea
jainautaedu <146860864+jainautaedu@users.noreply.github.com>
2023-10-05 11:48:05 -0500

    Delete getintegrand2.m

--- getintegrand2.m
+++
@@ -1,7 +0,0 @@
-function getintegrand2=getintegrand2(xbarstar,abar,Bimax,lambda,m)
-    
-for ii=1:length(xbarstar)
-        getintegrand2(ii)=Bi0(xbarstar(ii),abar,Bimax)*cos(lambda(m)*xbarstar(ii));
-end
-
-end
